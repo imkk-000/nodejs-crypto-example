80f264e0a11391aaebf0717e2e5af5003ea9ccbb
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2019-04-11 12:41:53 +0700

    [Edited]: move key to const SECRET_KEY

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+const SECRET_KEY = 'this is my secret key.'
 const crypto = require('crypto')
-const hmacSHA256 = crypto.createHmac('sha256', 'this is my secret key.')
+const hmacSHA256 = crypto.createHmac('sha256', SECRET_KEY)
 
 hmacSHA256.write('hash data')
 hmacSHA256.end()
+
 const hashData = hmacSHA256.read()
+// afdcf2494192a38859861e224f2abd2c995edc45ef0be2f4e7a215bd52e18c97
 console.log(hashData.toString('hex'))
